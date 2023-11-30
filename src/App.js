72d2351bfd21847f05cc0b03bc74915fd971af17
SimoBekacem <SimoBekacem>
2023-11-30 17:56:16 +0100

    creating the header Profile

--- src/App.js
+++ src/App.js
@@ -1,22 +1,14 @@
 import React from 'react';
+import { CssBaseline } from '@mui/material';
+
+import RolesCreatoPage from './features/Admin_RolesCreator/pages/RolesCreator.page';
 
 function App() {
 	return (
-		<div className='App'>
-			<header className='App-header'>
-				<p>
-					Edit <code>src/App.js</code> and save to reload.
-				</p>
-				<a
-					className='App-link'
-					href='https://reactjs.org'
-					target='_blank'
-					rel='noopener noreferrer'
-				>
-					Learn React
-				</a>
-			</header>
-		</div>
+		<>
+			<CssBaseline />
+			<RolesCreatoPage />
+		</>
 	);
 }
 
