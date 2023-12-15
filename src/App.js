cdc75726bd821beae36c7a4b4ff0a8569df1b134
SimoBekacem <SimoBekacem>
2023-12-15 11:41:03 +0100

    the admin and land-page are good in frent-end

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { CssBaseline } from '@mui/material';
 
-import RolesCreatoPage from './features/Admin_RolesCreator/pages/RolesCreator.page';
 import Landing_page from './features/Landing_page/pages/Landing_page.page';
+import ProfileCreatoPage from './features/Admin/Admin_ProfileCreator/pages/ProfileCreator.page';
+import RolesCreatorPage from './features/Admin/Admin_RolesCreator/pages/RolesCreator.page';
 
 function App() {
 	return (
